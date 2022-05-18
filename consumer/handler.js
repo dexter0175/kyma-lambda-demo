73c66efd885ecc3e369f64e21c9996892c21ecba
Siarhei Bahamiaka <siarhei.bahamiaka@leverx.com>
2022-05-19 00:51:57 +0300

    fix deps

--- consumer/handler.js
+++ consumer/handler.js
@@ -1,8 +1,9 @@
-const TelegramBot = require("node-telegram-api");
+const TelegramBot = require('node-telegram-bot-api');
 const token = '5396955540:AAEOGjQkGQcguAI5Lx8t5AQf6DFAJkrBxdI';
 
 module.exports = {
   main: function (event, context) {
+    console.log(event);
     const tgBot = new TelegramBot(token, {polling: true});
     tgBot.sendMessage(640902100, event);
 
