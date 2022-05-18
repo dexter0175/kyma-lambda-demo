903799218408296b760827032ea33fbe78d5d89c
Siarhei Bahamiaka <siarhei.bahamiaka@leverx.com>
2022-05-19 01:02:10 +0300

    fix message format

--- consumer/handler.js
+++ consumer/handler.js
@@ -2,10 +2,11 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '5396955540:AAEOGjQkGQcguAI5Lx8t5AQf6DFAJkrBxdI';
 
 module.exports = {
-  main: function (event, context) {
+  main: async function (event, context) {
     console.log(event);
     const tgBot = new TelegramBot(token, {polling: true});
-    tgBot.sendMessage(640902100, event);
+
+    await tgBot.sendMessage(640902100, JSON.stringify(event, null, 2));
 
     return "Hello World from Kyma Serverless!";
   },
