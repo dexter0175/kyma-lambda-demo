90e7afe22a37fc5d22c6ca9ae79059a69954a3cc
Siarhei Bahamiaka <siarhei.bahamiaka@leverx.com>
2022-05-19 01:20:06 +0300

    fix bot instance location

--- consumer/handler.js
+++ consumer/handler.js
@@ -1,10 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '5396955540:AAEOGjQkGQcguAI5Lx8t5AQf6DFAJkrBxdI';
+const tgBot = new TelegramBot(token, {polling: true});
 
 module.exports = {
   main: async function (event, context) {
     console.log(event);
-    const tgBot = new TelegramBot(token, {polling: true});
 
     await tgBot.sendMessage(640902100, JSON.stringify({
       'ce-type': event['ce-type'],
