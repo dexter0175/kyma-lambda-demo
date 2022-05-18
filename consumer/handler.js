7bb4b77d0f8229b345016460c525dd99935aa345
Siarhei Bahamiaka <siarhei.bahamiaka@leverx.com>
2022-05-19 00:33:56 +0300

    fix

--- consumer/handler.js
+++ consumer/handler.js
@@ -4,7 +4,7 @@ const token = '5396955540:AAEOGjQkGQcguAI5Lx8t5AQf6DFAJkrBxdI';
 module.exports = {
   main: function (event, context) {
     const tgBot = new TelegramBot(token, {polling: true});
-    _tgBot.sendMessage(640902100, event);
+    tgBot.sendMessage(640902100, event);
 
     return "Hello World from Kyma Serverless!";
   },
